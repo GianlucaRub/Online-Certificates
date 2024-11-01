d6e92aac78b28644edcbaa715974041620b2b20d
Gianluca Ruberto <gianluca.ruberto@mail.polimi.it>
2024-11-01 16:37:24 +0100

    prettier formatter

--- javascript_programming_essentials/Scope/scope_lab.js
+++ javascript_programming_essentials/Scope/scope_lab.js
@@ -3,14 +3,12 @@ var globalVar = "I'm a global variable";
 let globalLet = "I'm also global, but scoped with let";
 const globalConst = "I'm a global constant";
 
-
 {
-// Block scope
-var blockVar = "I'm a block-scoped var";
-let blockLet = "I'm a block-scoped let";
-const blockConst = "I'm a block-scoped const";
+  // Block scope
+  var blockVar = "I'm a block-scoped var";
+  let blockLet = "I'm a block-scoped let";
+  const blockConst = "I'm a block-scoped const";
 }
-
 
 // Global scope
 console.log(globalVar); // Output: "I'm a global variable"
@@ -21,13 +19,13 @@ console.log(globalConst); // Output: "I'm a global constant"
 // console.log(blockVar);
 // console.log(blockLet);
 
-function show(){
-    var functionVar = "I'm a block-scoped var";
-    let functionLet = "I'm a block-scoped let";
-    const functionConst = "I'm a block-scoped const";
-    }
-    show();
-    
-    // console.log(functionVar); // Throws ReferenceError
-    // console.log(functionLet); // Throws ReferenceError
-    // console.log(functionConst); // Throws ReferenceError
+function show() {
+  var functionVar = "I'm a block-scoped var";
+  let functionLet = "I'm a block-scoped let";
+  const functionConst = "I'm a block-scoped const";
+}
+show();
+
+// console.log(functionVar); // Throws ReferenceError
+// console.log(functionLet); // Throws ReferenceError
+// console.log(functionConst); // Throws ReferenceError
--- javascript_programming_essentials/groceryTracker/grocery_tracker.js
+++ javascript_programming_essentials/groceryTracker/grocery_tracker.js
@@ -1,3 +1,5 @@
-function sumGroceries(grocery_1,grocery_2,resultID) {
-   document.getElementById(resultID).innerText = `Total grocery cost is: ${grocery_1+grocery_2}`;
-   }
+function sumGroceries(grocery_1, grocery_2, resultID) {
+  document.getElementById(resultID).innerText = `Total grocery cost is: ${
+    grocery_1 + grocery_2
+  }`;
+}
