3e606d8edea1fe50f0cdf3c24b9fd8f71359def2
Gianluca Ruberto <gianluca.ruberto@mail.polimi.it>
2024-12-17 14:06:28 +0100

    removed duplicated code

--- developing_front_end_apps_with_react/conference_event_planner/vite.config.js
+++ developing_front_end_apps_with_react/conference_event_planner/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Online-Certificates/developing_front_end_apps_with_react/conference_event_planner',
+  base: '/Online-Certificates/',
   plugins: [react()],
 });
